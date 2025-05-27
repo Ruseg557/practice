70b69d30d6cfa2a7d60b867d80c4c47077602aaa
Ruseg <abcsamprp@gmail.com>
2025-05-27 22:22:05 +0300

    Первая реализация GUI

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
@@ -9,37 +10,32 @@ import (
 	"strings"
 )
 
-//func merge(arr1, arr2 []int) []int {
-//	var i, j = 0, 0
-//	var c = []int{}
-//	for i < len(arr1) || j < len(arr2) {
-//		if i < len(arr1) && (j >= len(arr2) || arr1[i] < arr2[j]) {
-//			c = append(c, arr1[i])
-//			i++
-//		} else {
-//			c = append(c, arr2[j])
-//			j++
-//		}
-//	}
-//	return c
-//}
-//
-//func merge_sort(arr []int) []int {
-//	if len(arr) <= 1 {
-//		return arr
-//	}
-//	mid := len(arr) / 2
-//	var arr1 = merge_sort(arr[:mid])
-//	var arr2 = merge_sort(arr[mid:])
-//	return merge(arr1, arr2)
-//}
+func merge(arr1, arr2 []float32) []float32 {
+	var i, j = 0, 0
+	var c []float32
+	for i < len(arr1) || j < len(arr2) {
+		if i < len(arr1) && (j >= len(arr2) || arr1[i] < arr2[j]) {
+			c = append(c, arr1[i])
+			i++
+		} else {
+			c = append(c, arr2[j])
+			j++
+		}
+	}
+	return c
+}
+
+func mergeSort(arr []float32) []float32 {
+	if len(arr) <= 1 {
+		return arr
+	}
+	mid := len(arr) / 2
+	var arr1 = mergeSort(arr[:mid])
+	var arr2 = mergeSort(arr[mid:])
+	return merge(arr1, arr2)
+}
 
 func main() {
-	//var arr = []int{5, 2, -1, 0, 5, 16, 27, 8, 9, 10}
-	//var sortedArr = merge_sort(arr)
-	//fmt.Println(arr)
-	//fmt.Println(sortedArr)
-
 	myApp := app.New()
 	myWindow := myApp.NewWindow("Merge sort application")
 
@@ -53,27 +49,53 @@ func main() {
 	mainLabel := widget.NewLabel("Сортировка методом слияния")
 	input := widget.NewEntry()
 	input.SetPlaceHolder("Введите значения через пробел")
-	resultLabel := widget.NewLabel("Здесь будет результат сортировки")
+	errLabel := widget.NewLabel("Здесь будут сообщения об ошибках")
+	resLabel := widget.NewLabel("Здесь будет результат сортировки")
+
+	// Кнопка сортировки
 	sortButton := widget.NewButton("Отсортировать", func() {
 		inputText := input.Text
 
 		numStrings := strings.Split(inputText, " ")
-		var numbers []int
-		for _, s := range numStrings {
-			num, err := strconv.Atoi(strings.TrimSpace(s))
-			if err != nil {
-				resultLabel.SetText("ERR")
-				return
-			}
-			numbers = append(numbers, num)
+		var numbers []float32
+		var flagErr = false
+		if inputText == "" { // Проверка на пустоту ввода
+			errLabel.SetText("Ничего не введнено")
+			return
 		}
+
+		for _, s := range numStrings { // Обработка ввода
+			num, err := strconv.ParseFloat(strings.TrimSpace(s), 32)
+			if err == nil {
+				numbers = append(numbers, float32(num))
+			}
+			if err != nil { // Запоминаем, есть ли недопустимые символы
+				flagErr = true
+			}
+		} // for
+
+		if flagErr { // Вывод сообщения об ошибках (кроме пустоты)
+			errLabel.SetText("Недопустимые символы не обнаружены")
+		} else {
+			errLabel.SetText("Недопустимые символы. Программа их проигнорировала")
+		}
+
+		var resArr = mergeSort(numbers)
+
+		if resArr != nil { // Проверка на существование результата
+			resLabel.SetText(fmt.Sprint(resArr))
+		} else {
+			resLabel.SetText("Здесь будет результат сортировки")
+		}
+
 	}) // sortButton
 
 	myWindow.SetContent(container.NewVBox(
 		mainLabel,
 		input,
 		sortButton,
-		resultLabel,
+		errLabel,
+		resLabel,
 	))
 
 	myWindow.ShowAndRun()
